9b00dabd0e6f09ed9c0cf1fc0dc073e6f84da63f
CinderBelle012 <167465429+CinderBelle012@users.noreply.github.com>
2024-06-04 16:37:36 -0300

    Aula- 04/06
    aula alinhar informações

--- GestorDeEstudantesT6/FormInserirEstudante.cs
+++ GestorDeEstudantesT6/FormInserirEstudante.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Mysqlx.Expect;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +18,53 @@ namespace GestorDeEstudantesT6
         {
             InitializeComponent();
         }
+
+        private void buttonEnviarFoto_Click(object sender, EventArgs e)
+        {
+            //exibe uma janela para procurar a imagem 
+            OpenFileDialog selecionarImagem = new OpenFileDialog();
+            selecionarImagem.Filter = "Selecione a foto (*.jpg; png;*.gif)| *.jpg;|*.png;*gif";
+           if (selecionarImagem.ShowDialog() == DialogResult.OK)
+            {
+                pictureBoxUsuario.Image = Image.FromFile(selecionarImagem.FileName);   
+            }
+
+        }
+
+        private void buttonCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        bool Verificar()
+        {
+            if((textBoxNome.Text.Trim() == "") ||
+              (textBoxSobrenome.Text.Trim() == "") ||
+              (textBoxTelefone.Text.Trim() == "") ||
+              (textBoxEndereço.Text.Trim() == "") ||
+              (pictureBoxUsuario.Image == null))
+              {
+                return false;   
+              }
+            else
+            {
+                return true;
+            }
+        }
+
+        private void buttonCadastrar_Click(object sender, EventArgs e)
+        {
+            //Cria um estudante 
+            Estudante estudnte = new Estudante();
+            //Variáveis auxiliares.
+
+            String nome = textBoxNome.Text; 
+            String spbrenome = textBoxSobrenome.Text; 
+            DateTime nascimento = dateTimePickerNascimento.Value
+            String telefone = textBoxTelefone.Text;
+            string endereço = textBoxEndereço.Text;
+            string genero = "Femenino";
+        }
     }
-}
+    }
+    
