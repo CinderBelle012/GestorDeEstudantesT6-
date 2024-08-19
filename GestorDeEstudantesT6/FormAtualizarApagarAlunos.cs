090a3afcb5bf2c34c46dfbc66a8502306e9783a3
CinderBelle012 <167465429+CinderBelle012@users.noreply.github.com>
2024-08-19 17:24:26 -0300

    aTUALIZAÇÃO DO PROJETO
    aTUALIZAÇÃO DO PROJETO

--- GestorDeEstudantesT6/FormAtualizarApagarAlunos.cs
+++ GestorDeEstudantesT6/FormAtualizarApagarAlunos.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -136,6 +137,46 @@ namespace GestorDeEstudantesT6
             else
             {
                 return true;
+            }
+        }
+
+        private void buttonBuscar_Click(object sender, EventArgs e)
+        {
+            // Busca estudante pela ID.
+            int id = Convert.ToInt32(textBoxId.Text);   
+            MeuBancoDeDados meuBancoDeDados = new MeuBancoDeDados();
+
+            MySqlCommand comando = new MySqlCommand("SELECT ´ id´,´none`,´sobrenome´,`nascimento´,`genero´,`telefone´,`endereco´,`foto´,`FROM´,`estudantes´, `WHERE` `id` ="= + id,
+                meuBancoDeDados.getConexao);
+
+            DataTable tabela = estudante.getEstudantes(comando);
+
+            if (tabela.Rows.Count > 0)
+            {
+                textBoxNome.Text = tabela.Rows[0]["nome"].ToString();
+                textBoxSobrenome.Text = tabela.Rows[0]["sobrenome"].ToString();
+                textBoxTelefone.Text = tabela.Rows[0]["telefone"].ToString();
+                textBoxEndereco.Text = tabela.Rows[0]["endereco"].ToString();
+
+                dateTimePickerNascimento.Value = (DateTime)tabela.Rows[0]["nascimento"];
+
+                if (tabela.Rows[0]["genero"].ToString() == "Feminino")
+                {
+                    radioButtonFeminino.Checked = true;
+                }
+                else
+                {
+                    radioButtonMasculino.Checked = false;   
+                }
+
+                // A foto
+                byte[] image = (byte[])tabela.Rows[0]["fotos"];
+                //"objeto" intermediario entre a foto que está na tabela.
+                // e a foto que está salva no banco de dados. 
+                MemoryStream FotoDoAluno = new MemoryStream(imagem);
+                //resconstroi a imagem base em um "memory Stream"
+                pictureBoxFoto.Image = image.FromStream(FotoDoAluno);
+
             }
         }
     }
